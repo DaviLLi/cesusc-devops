c6ba48575298675d17fbdfde7e92aaa72f87193c
Davi-Coren <davi.landal@corensc.gov.br>
2026-04-24 11:05:36 -0300

    instalacao selenium, configuracao ambiente de testes e criacao testes iniciais

---
+++ tests/sum.test.js
@@ -0,0 +1,15 @@
+const sum = require("../src/sum");
+
+jest.setTimeout(30000);
+
+test("adds 1 + 2 to equal 3", () => {
+  expect(sum([1, 2])).toBe(3);
+});
+
+test("adds -1 + 1 to equal 0", () => {
+  expect(sum([-1, 1])).toBe(0);
+});
+
+test("adds 1, 2, 3, 4, 5 to equal 15", () => {
+  expect(sum([1, 2, 3, 4, 5])).toBe(15);
+});
