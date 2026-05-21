c849a05a3516fe33c42b38b84d0e60e691b989f5
DaviLLi <davilandal10@gmail.com>
2026-05-21 19:59:44 -0300

    const express e app

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 // Essa é a forma de implementar este endpoint
+const express = require("express");
+const app = express();
 
-app.get('/', (req, res) => {
-  res.sendFile('views/index.html', { root: __dirname });
+app.get("/", (req, res) => {
+  res.sendFile("views/index.html", { root: __dirname });
 });
