1bbd8daa7adc3db4d33ab0e4c793e2cf0f39a670
DaviLLi <davilandal10@gmail.com>
2026-06-12 20:32:11 -0300

    Corrige teste Selenium para GitHub Actions

--- tests/google.test.js
+++ tests/google.test.js
@@ -1,7 +1,17 @@
 const { Builder, By, until } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
 
 async function testGoogle() {
-  let driver = await new Builder().forBrowser("chrome").build();
+  const options = new chrome.Options();
+
+  options.addArguments("--headless");
+  options.addArguments("--no-sandbox");
+  options.addArguments("--disable-dev-shm-usage");
+
+  let driver = await new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(options)
+    .build();
 
   try {
     await driver.get("https://www.google.com");
